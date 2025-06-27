66a22774cb4eb1ce73d6233f77a70cc2eb05e928
AndriyNyk0lyn <andriy.nykolyn@bitsorchestra.com>
2025-06-27 23:40:48 +0300

    Add input, select, and slider components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import { Button } from "@/components/ui/button";
 import { Header } from "@/components/Header";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
       <Header />
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>Click me</Button>
-      </div>
     </ThemeProvider>
   );
 }
